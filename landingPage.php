ba3f0ca58fe08600132f4849030a884e8e02aee9
Gregory King <gregory_king1995@hotmail.com>
2020-05-21 02:08:01 -0500

    Fixed Buttons on Landing Page

--- landingPage.php
+++ landingPage.php
@@ -63,35 +63,32 @@ Session_start();//continue session
 
 <!-- navbar end -->
 
-
-
-
 <!-- carousel-->
 <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
-	    <ol class="carousel-indicators">
-	    	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-	    	<li data-target="#myCarousel" data-slide-to="1"></li>
-	    	<li data-target="#myCarousel" data-slide-to="2"></li>
-	    </ol>
-  	<div class="carousel-inner">
-	    <div class="carousel-item active" data-interval="5000">
-	      <img class="carousel-img" src="images/carousel-2.jpg" >
-	      <div class="carousel-caption d-md-block">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="5000">
+      <img class="carousel-img" src="images/carousel-2.jpg" >
+      <div class="carousel-caption d-md-block">
 
-	      </div>
-    	</div>
-	    <div class="carousel-item">
-	      <img class="carousel-img" src="images/carousel-1.jpg">
-	      <div class="carousel-caption d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="carousel-img" src="images/carousel-1.jpg">
+      <div class="carousel-caption d-md-block">
 
-	      </div>
-	    </div>
-	    <div class="carousel-item">
-	      <img class="carousel-img" src="images/carousel-3.jpg" >
-	      <div class="carousel-caption d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="carousel-img" src="images/carousel-3.jpg" >
+      <div class="carousel-caption d-md-block">
 
-	      </div>
-	    </div>
+      </div>
+    </div>
   </div>
   <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
