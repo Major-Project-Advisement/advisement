c1f1f464d883b25bdac7c7f8223642fccb411791
KeroneC <kerone.ant.creary@hotmail.com>
2020-07-05 00:12:23 -0500

    fixed modal issue

--- studentTemplate.php
+++ studentTemplate.php
@@ -1,3 +1,30 @@
+<?php
+    $date = date('Y/m/d');
+
+    //Create a SQL statement to get the Advisors name using $AdvisorID and set it to a variable to use for the modal
+    $sql = "SELECT * FROM `advisor` WHERE `AdvisorID` = '".$AdvisorID."' LIMIT 1 ";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    $AdvisorFirstName = $row['FirstName'];
+    $AdvisorLastName = $row['LastName'];
+    $AdvisorImage = $row['Image'];
+    if($AdvisorImage == 'null'){
+      $AdvisorImage = 'images/placeholder.jpg';
+    }else{
+      $AdvisorImage = 'uploads/'.$AdvisorImage;
+    }
+    $AdvisorPhone = $row['Phone'];
+    $AdvisorEmail = $row['Email'];
+    $AdvisorTitle = $row['Title'];
+    $AdvisorSchool = $row['SchoolID'];
+
+    if($AdvisorSchool == 1){
+      $AdvisorSchool = "School of Computing and Information Technology";
+    }else{
+      $AdvisorSchool = "School of Engineering";
+    }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -151,6 +178,125 @@
 
 </footer>
 <!-- Footer end -->
+
+<?php
+echo '
+<div class="modal fade" id="CreateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <form action="studentMessage.php" method="post" id="register_form" enctype="multipart/form-data">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Create Message</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="form-group">
+                      <label>From: <b>Me</b></label>
+                      <br>
+                      <label>To: <b>'.$AdvisorFirstName.' '.$AdvisorLastName.'</b> </label>
+                    </div>  
+                    <div class="form-group">
+                      <label>Subject</label>
+                      <input type="text" name="subject" class="form-control" placeholder="Subject">
+                    </div>
+                    <div class="form-group">
+                      <label>Message</label>
+                      <input type="text" name="message" class="form-control" placeholder="Message">
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal fade" id="RequestMeeting" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <form action="studentMeeting.php" method="post" id="register_form" enctype="multipart/form-data">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Request A Meeting</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="form-group">
+                      <label>Time: <br><input type="time" id="MeetingTime" name="MeetingTime"
+                      min="09:00" max="18:00" required> </label>
+                      <br>
+                      <label>Date: <br><input type="date" id="MeetingDate" name="MeetingDate"
+                      value="'.$date.'"
+                      min="'.$date.'" ></label>
+                    </div>  
+                    <div class="form-group">
+                      <label>Topic:<br></label>
+                      <select id="topic" name="topic" class="form-control">
+                        <option value="Grades">Issue With Grades</option>
+                        <option value="Course">Course Selection</option>
+                        <option value="Personal">Personal Challlenges</option>
+                        <option value="Financial">Financial Challenges</option>
+                        <option value="Explanation">Explanation of Rules and Procedures</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label>Description:<br></label>
+                      <input type="text" name="description" class="form-control" placeholder="Description">
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Request Meeting</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal fade" id="ViewAdvisor" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <form action="viewAdvisor.php" method="post" id="register_form" enctype="multipart/form-data">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">View Advisor</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="profile-userpic text-center">
+                      <img src="'.$AdvisorImage.'" class="img-responsive" alt="profile picture">
+                    </div>
+                    <div class="text-center">
+                      '.$AdvisorTitle.' '.$AdvisorFirstName.' '.$AdvisorLastName.'
+                    </div>
+                    <div class="text-center">
+                      '.$AdvisorEmail.'
+                    </div>
+                    <div class="text-center">
+                      '.$AdvisorPhone.'
+                    </div>
+                    <div class="text-center">
+                      '.$AdvisorSchool.'
+                    </div>
+                    
+                    
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>';
+?>
+
+          
 
 
     
